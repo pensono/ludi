c730fc04229a890d9c4b87165974c10b7ea824e6
Ethan Shea <ethan.shea1@gmail.com>
2023-10-19 23:33:51 -0400

    Enable vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import { Toaster } from 'svelte-french-toast';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
