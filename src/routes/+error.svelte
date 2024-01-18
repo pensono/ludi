a1cdb98cd7d889bbaf7d3ea6f206bb3f75dc0260
Ethan Shea <ethan.shea1@gmail.com>
2024-01-17 23:09:48 -0500

    Better error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+	import SingleLayout from "$lib/components/layout/SingleLayout.svelte";
+</script>
+
+<!-- Error pages must be simple -->
+<SingleLayout>
+    <h1>{$page.status}: {$page.error?.message}</h1>
+</SingleLayout>
