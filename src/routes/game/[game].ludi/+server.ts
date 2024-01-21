0eec8feaf3968e263ffcbba9efb6275f388c9ddc
Ethan Shea <ethan.shea1@gmail.com>
2024-01-20 22:41:35 -0500

    Fix type error

--- src/routes/game/[game].ludi/+server.ts
+++ src/routes/game/[game].ludi/+server.ts
@@ -5,7 +5,7 @@ import type { RouteParams } from "./$types";
 
 export async function GET({ params }: { params: RouteParams}) {
   const convex = getConvexClient();
-  const result = await convex.query(api.rules.get, { idOrName: params.game });
+  const result = await convex.query(api.rules.get, { name: params.game });
   
   return new Response(result.source);
 }
